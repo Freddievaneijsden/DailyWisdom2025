5515517f9aee01be410ed116e20b0d273526e7f5
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-03-18 17:02:54 +0100

    Add addDailyWisdom and findTopByOrderByIdDesc

--- src/main/java/com/example/springboot2025/DailyWisdomController.java
+++ src/main/java/com/example/springboot2025/DailyWisdomController.java
@@ -1,10 +1,16 @@
 package com.example.springboot2025;
 
+import com.example.springboot2025.domain.entity.DailyWisdom;
 import com.example.springboot2025.infrastructure.persistence.DailyWisdomRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class DailyWisdomController {
@@ -22,7 +28,16 @@ public class DailyWisdomController {
 
     @RequestMapping("dailywisdom")
     String getAllDailyWisdom (Model model) {
-        model.addAttribute("dailyWisdomRepository", dailyWisdomRepository.findAll());
+        DailyWisdom latestWisdom = dailyWisdomRepository.findTopByOrderByIdDesc().orElse(null);
+        model.addAttribute("dailyWisdomRepository", latestWisdom);
         return "seeMessage";
+    }
+
+    @PostMapping("/addMessage")
+    public String addDailyWisdom(@RequestParam String message) {
+        DailyWisdom newMessage = new DailyWisdom();
+        newMessage.setMessage(message);
+        dailyWisdomRepository.save(newMessage);
+        return "redirect:/";
     }
 }
--- src/main/java/com/example/springboot2025/infrastructure/persistence/DailyWisdomRepository.java
+++ src/main/java/com/example/springboot2025/infrastructure/persistence/DailyWisdomRepository.java
@@ -3,5 +3,10 @@ package com.example.springboot2025.infrastructure.persistence;
 import com.example.springboot2025.domain.entity.DailyWisdom;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.Optional;
+
 public interface DailyWisdomRepository extends ListCrudRepository<DailyWisdom, Integer> {
+
+    Optional<DailyWisdom> findTopByOrderByIdDesc();
+
 }
